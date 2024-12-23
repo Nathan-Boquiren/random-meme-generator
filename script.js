a312d17e48b0c7aaef193255d3e0c1f2160f652c
Nathan Boquiren <nathanjboquiren@gmail.com>
2024-12-23 09:10:07 -0500

    made it like a social media feed :)

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ let cl = console.log;
 const generateMemeBtn = document.getElementById("generate-meme-btn");
 const memeContainer = document.getElementById("meme-container");
 
-generateMemeBtn.addEventListener("click", fetchMemeInfo);
-
+let memes = [];
 function fetchMemeInfo() {
-  fetch("https://meme-api.com/gimme")
+  fetch("https://meme-api.com/gimme/programminghumor")
     .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -14,10 +13,28 @@ function fetchMemeInfo() {
       return response.json();
     })
     .then((data) => {
-      cl(data.url);
-      memeContainer.innerHTML = `<img id="meme-image" src="${data.url}" ></img>`;
+      cl(data);
+      if (!memes.includes(data.url)) {
+        memes.push(data.url);
+        memeContainer.innerHTML += `<div class="image-card"><h2>${data.title}</h2><img id="meme-image" src="${data.url}" ></div>`;
+      }
     })
     .catch((error) => {
       console.error("There was an error:", error);
     });
 }
+
+fetchMemeInfo();
+
+document.addEventListener("touchstart", (event) => {
+  startY = event.touches[0].clientY;
+});
+
+document.addEventListener("touchend", (event) => {
+  const endY = event.changedTouches[0].clientY;
+  const deltaY = startY - endY;
+
+  if (deltaY > 50) {
+    fetchMemeInfo();
+  }
+});
